94061b9795b0f789641141b255573ad0ed45c320
Hưng Trần <hungbkit128@gmail.com>
2018-12-01 17:53:08 +0700

    hungtv cm

---
+++ TableViewDemo/TableViewDemo/AutoHeightRow/AutoHeightCell.swift
@@ -0,0 +1,26 @@
+//
+//  AutoHeightCell.swift
+//  TableViewDemo
+//
+//  Created by Vtsoft2 on 12/1/18.
+//  Copyright © 2018 hungtv64. All rights reserved.
+//
+
+import UIKit
+
+class AutoHeightCell: UITableViewCell {
+
+    @IBOutlet weak var nameLB: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TableViewDemo/TableViewDemo/AutoHeightRow/AutoHeightRowVC.swift
+++ TableViewDemo/TableViewDemo/AutoHeightRow/AutoHeightRowVC.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class AutoHeightRowVC: UIViewController {
+class AutoHeightRowVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names:[String] = ["tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung ",
+                          "tran van hung tran van hung tran van hung tran van hung tran van hung tran van hung "]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        
+        tableView.register(UINib(nibName: "AutoHeightCell", bundle: Bundle.main), forCellReuseIdentifier: "AutoHeightCell")
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 600
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +40,17 @@ class AutoHeightRowVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AutoHeightCell", for: indexPath) as! AutoHeightCell
+        cell.nameLB.text = names[indexPath.row]
+        return cell
+    }
 }
