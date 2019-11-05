debdf6035c012f663179bf6c24d68d32569ba0e4
Tran Van Hung <hungbkit128@gmail.com>
2019-11-06 01:40:03 +0700

    htv

---
+++ SqlLiteDemo/SqlLiteDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SqlLiteDemo
+//
+//  Created by Tran Van Hung on 10/26/19.
+//  Copyright © 2019 hungtv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
