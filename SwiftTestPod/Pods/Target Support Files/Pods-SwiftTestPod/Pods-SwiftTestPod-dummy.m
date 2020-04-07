ffd2d25bf007c407854fc88a883351297f112e32
Tran Van Hung <hungbkit128@gmail.com>
2020-04-07 19:46:10 +0700

    hungtv cmit

---
+++ SwiftTestPod/Pods/Target Support Files/Pods-SwiftTestPod/Pods-SwiftTestPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftTestPod : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftTestPod
+@end
---
+++ SwiftTestPod/SwiftTestPod/TabbarAnimation/TabbarVC.swift
@@ -0,0 +1,30 @@
+//
+//  TabbarVC.swift
+//  SwiftTestPod
+//
+//  Created by Tran Van Hung on 4/4/20.
+//  Copyright © 2020 hba. All rights reserved.
+//
+
+import UIKit
+
+class TabbarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
