be7e958501a95bea3617f99841ae6c0b8913d7a9
Trần Văn Hưng <hungbkit128@gmail.com>
2020-04-07 17:36:19 +0700

    htv

---
+++ SwiftPasscodeLock-master/PasscodeLockDemo/PasscodeLockConfiguration.swift
@@ -0,0 +1,26 @@
+//
+//  PasscodeLockConfiguration.swift
+//  PasscodeLock
+//
+//  Created by Yanko Dimitrov on 8/29/15.
+//  Copyright © 2015 Yanko Dimitrov. All rights reserved.
+//
+
+import Foundation
+import PasscodeLock
+
+struct PasscodeLockConfiguration: PasscodeLockConfigurationType {
+    let repository: PasscodeRepositoryType
+    let passcodeLength = 8
+    var isTouchIDAllowed = true
+    let shouldRequestTouchIDImmediately = true
+    let maximumIncorrectPasscodeAttempts = -1
+
+    init(repository: PasscodeRepositoryType) {
+        self.repository = repository
+    }
+
+    init() {
+        self.repository = UserDefaultsPasscodeRepository()
+    }
+}
